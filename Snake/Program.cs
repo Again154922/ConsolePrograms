e1d3b3242e2f801e1bef720d5ff557ab3aa2d44d
Again154922 <jamesjfy@163.com>
2026-09-26 10:26:03 +0800

    Snake 改为单线程 Stopwatch 游戏循环，方向判定合并成查表

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Snake;
+﻿using System.Diagnostics;
+
+namespace Snake;
 
 internal static class Program
 {
@@ -15,39 +17,45 @@ internal static class Program
 
     private static int[][] _map = null!;
     private static bool _gameStart;
-    private static Lock _lock = new();
     
-    private static Task _timer = null!;
-    private static Task _input = null!;
-    private static bool _move;
-    private static bool _didMove;
+    private static int _speed;
     private static int _dir = Right;
     private static List<(int, int)> _snake = new() { (8, 9), (8, 8), (8, 7) };
     private static (int, int) _food;
 
     private static Random _random = new();
+
+    // 方向 -> 行列增量：四个方向共用一份查表，不用每个方向抄一遍一样的逻辑
+    private static readonly Dictionary<int, (int Row, int Col)> Deltas = new()
+    {
+        [Up] = (-1, 0),
+        [Left] = (0, -1),
+        [Down] = (1, 0),
+        [Right] = (0, 1),
+    };
     
-    private static async Task Main(string[] args)
+    private static async Task Main()
     {
         Init(ref _map, _snake, ref _food);
 
+        // 单线程游戏循环：节拍用秒表自己算，不再需要计时线程、锁和标志位
+        var clock = Stopwatch.StartNew();
+        long lastMove = 0;
+
         while (_gameStart)
         {
-            lock (_lock)
+            if (Console.KeyAvailable)
             {
-                if (_move)
-                {
-                    Move(ref _map, _dir, ref _snake, ref _food);
-                    _move = false;
-                    _didMove = true;
-                }
+                HandleKey(Console.ReadKey(true).Key);
             }
-            if (_didMove)
+
+            if (clock.ElapsedMilliseconds - lastMove >= _speed)
             {
+                lastMove = clock.ElapsedMilliseconds;
+                Move(ref _map, _dir, ref _snake, ref _food);
                 ShowMap(_map);
-                _didMove = false;
             }
-            
+
             await Task.Delay(10);
         }
 
@@ -81,7 +89,7 @@ internal static class Program
     {
         Console.Write("输入游戏难度(1-5),默认为3 >>> ");
         string? input = Console.ReadLine();
-        int speed = int.Parse(input is "1" or "2" or "3" or "4" or "5" ? input : "3") switch
+        _speed = int.Parse(input is "1" or "2" or "3" or "4" or "5" ? input : "3") switch
         {
             1 => 1000,
             2 => 750,
@@ -93,26 +101,28 @@ internal static class Program
 
         Console.CursorVisible = false;
         
-        map = 
-        [
-            [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
-            [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-        ];
+        // map = 
+        // [
+        //     [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+        //     [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
+        // ]; 
+        SetMap(ref map, snake, (1, 1));
+        map[1][1] = Empty;
         SetFood(_random, map, ref food);
         SetMap(ref map, snake, food);
         ShowMap(map);
@@ -124,125 +134,69 @@ internal static class Program
         Console.Clear();
         ShowMap(map);
         
-        _timer = Task.Run(async () =>
-        {
-            do
-            {
-                await Task.Delay(speed);
-                lock (_lock) _move = true;
-            } while (_gameStart);
-        });
-        
-        _input = Task.Run(async () =>
-        {
-            do
-            {
-                await Task.Delay(10);
-                if (!Console.KeyAvailable) continue;
-                var key = Console.ReadKey(true).Key;
-                lock (_lock)
-                {
-                    _dir = key switch
-                    {
-                        ConsoleKey.UpArrow or ConsoleKey.W =>
-                            _map[_snake[0].Item1 - 1][_snake[0].Item2] is Empty or Food ||
-                            (_snake[0].Item1 - 1, _snake[0].Item2) == snake[^1]
-                                ? Up
-                                : _dir,
-                        ConsoleKey.LeftArrow or ConsoleKey.A =>
-                            _map[_snake[0].Item1][_snake[0].Item2 - 1] is Empty or Food ||
-                            (_snake[0].Item1, _snake[0].Item2 - 1) == snake[^1]
-                                ? Left
-                                : _dir,
-                        ConsoleKey.DownArrow or ConsoleKey.S =>
-                            _map[_snake[0].Item1 + 1][_snake[0].Item2] is Empty or Food ||
-                            (_snake[0].Item1 + 1, _snake[0].Item2) == snake[^1]
-                                ? Down
-                                : _dir,
-                        ConsoleKey.RightArrow or ConsoleKey.D =>
-                            _map[_snake[0].Item1][_snake[0].Item2 + 1] is Empty or Food ||
-                            (_snake[0].Item1, _snake[0].Item2 + 1) == snake[^1]
-                                ? Right
-                                : _dir,
-                        ConsoleKey.Escape => 0,
-                        _ => _dir
-                    };
-                }
-            } while (_gameStart);
-        });
-        
         _gameStart = true;
+    }
+
+    // 按键 -> 方向：目标格可走（或那格是下一步要让位的尾巴）才转向，否则保持原方向
+    private static void HandleKey(ConsoleKey key)
+    {
+        _dir = key switch
+        {
+            ConsoleKey.UpArrow or ConsoleKey.W => Turn(Up, _snake),
+            ConsoleKey.LeftArrow or ConsoleKey.A => Turn(Left, _snake),
+            ConsoleKey.DownArrow or ConsoleKey.S => Turn(Down, _snake),
+            ConsoleKey.RightArrow or ConsoleKey.D => Turn(Right, _snake),
+            ConsoleKey.Escape => 0,
+            _ => _dir
+        };
+    }
+
+    private static int Turn(int dir, List<(int, int)> snake)
+    {
+        if (!TryGetNext(snake[0], dir, out (int Row, int Col) next))
+            return _dir;
+
+        return _map[next.Row][next.Col] is Empty or Food || (next.Row, next.Col) == snake[^1]
+            ? dir
+            : _dir;
+    }
+
+    // 四个方向共用的一套计算：把方向换成下一格坐标，方向无效（0）时返回 false
+    private static bool TryGetNext((int Row, int Col) head, int dir, out (int Row, int Col) next)
+    {
+        if (Deltas.TryGetValue(dir, out (int Row, int Col) delta))
+        {
+            next = (head.Row + delta.Row, head.Col + delta.Col);
+            return true;
+        }
+
+        next = head;
+        return false;
     }
 
     private static void Move(ref int[][] map, int dir, ref List<(int, int)> snake, ref (int, int) food)
     {
-        switch (dir)
+        // 方向为 0（按了 Esc）或不是有效方向时结束游戏
+        if (!TryGetNext(snake[0], dir, out (int Row, int Col) next))
         {
-            case Up:
-                if (map[snake[0].Item1 - 1][snake[0].Item2] is Body or Wall && snake.IndexOf((snake[0].Item1 - 1, snake[0].Item2)) != snake.Count - 1)
-                {
-                    _gameStart = false;
-                    return;
-                }
-                
-                if (map[snake[0].Item1 - 1][snake[0].Item2] != Food)
-                    snake.RemoveAt(snake.Count - 1);
-                else if (GetEmpty(map).Count != 0)
-                    SetFood(_random, map, ref food);
-                snake.Insert(0, (snake[0].Item1 - 1, snake[0].Item2));
-                
-                break;
-            
-            case Left:
-                if (map[snake[0].Item1][snake[0].Item2 - 1] is Body or Wall && snake.IndexOf((snake[0].Item1, snake[0].Item2 - 1)) != snake.Count - 1)
-                {
-                    _gameStart = false;
-                    return;
-                }
-                
-                if (map[snake[0].Item1][snake[0].Item2 - 1] != Food)
-                    snake.RemoveAt(snake.Count - 1);
-                else if (GetEmpty(map).Count != 0)
-                    SetFood(_random, map, ref food);
-                snake.Insert(0, (snake[0].Item1, snake[0].Item2 - 1));
-                
-                break;
-            
-            case Down:
-                if (map[snake[0].Item1 + 1][snake[0].Item2] is Body or Wall && snake.IndexOf((snake[0].Item1 + 1, snake[0].Item2)) != snake.Count - 1)
-                {
-                    _gameStart = false;
-                    return;
-                }
-                
-                if (map[snake[0].Item1 + 1][snake[0].Item2] != Food)
-                    snake.RemoveAt(snake.Count - 1);
-                else if (GetEmpty(map).Count != 0)
-                    SetFood(_random, map, ref food);
-                snake.Insert(0, (snake[0].Item1 + 1, snake[0].Item2));
-
-                break;
-            
-            case Right:
-                if (map[snake[0].Item1][snake[0].Item2 + 1] is Body or Wall && snake.IndexOf((snake[0].Item1, snake[0].Item2 + 1)) != snake.Count - 1)
-                {
-                    _gameStart = false;
-                    return;
-                }
-                
-                if (map[snake[0].Item1][snake[0].Item2 + 1] != Food)
-                    snake.RemoveAt(snake.Count - 1);
-                else if (GetEmpty(map).Count != 0)
-                    SetFood(_random, map, ref food);
-                snake.Insert(0, (snake[0].Item1, snake[0].Item2 + 1));
-                
-                break;
-            
-            default:
-                _gameStart = false;
-                return;
+            _gameStart = false;
+            return;
         }
-        
+
+        // 撞墙或撞到自己就结束；尾巴下一步会让位，所以踩尾巴不算死
+        if (map[next.Row][next.Col] is Body or Wall &&
+            snake.IndexOf((next.Row, next.Col)) != snake.Count - 1)
+        {
+            _gameStart = false;
+            return;
+        }
+
+        if (map[next.Row][next.Col] != Food)
+            snake.RemoveAt(snake.Count - 1);
+        else if (GetEmpty(map).Count != 0)
+            SetFood(_random, map, ref food);
+        snake.Insert(0, (next.Row, next.Col));
+
         SetMap(ref map, snake, food);
     }
     
@@ -262,7 +216,6 @@ internal static class Program
             if (!win) break;
         }
         Console.WriteLine(win ? "恭喜你，你赢了!" : "游戏结束!");
-        Task.WaitAll([_input, _timer], 100);
         Console.WriteLine("按任意键退出...");
         Console.ReadKey(true);
     }
